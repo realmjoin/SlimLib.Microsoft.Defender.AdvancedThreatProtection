0ab024e712b3235ca169e7cba706fb2390431d17
Jan Berdel <jan.berdel@glueckkanja.com>
2020-09-21 10:51:10 +0200

    Users only returns related machines
    oops

--- SlimAtp/ClientInterfaces/ISlimAtpUserClient.cs
+++ SlimAtp/ClientInterfaces/ISlimAtpUserClient.cs
@@ -1,16 +1,12 @@
 using SlimAtp.Auth;
-using System;
 using System.Collections.Generic;
 using System.Text.Json;
 using System.Threading;
-using System.Threading.Tasks;
 
 namespace SlimAtp
 {
     public interface ISlimAtpUserClient
     {
-        Task<JsonElement> GetUserAsync(IAzureTenant tenant, Guid id, ScalarRequestOptions? options = default, CancellationToken cancellationToken = default);
-
-        IAsyncEnumerable<JsonElement> ListUsersAsync(IAzureTenant tenant, ListRequestOptions? options = default, CancellationToken cancellationToken = default);
+        IAsyncEnumerable<JsonElement> ListUserMachinesAsync(IAzureTenant tenant, string id, ListRequestOptions? options = default, CancellationToken cancellationToken = default);
     }
 }
--- SlimAtp/Impl/SlimAtpClientImpl.User.cs
+++ SlimAtp/Impl/SlimAtpClientImpl.User.cs
@@ -1,25 +1,16 @@
 ﻿using SlimAtp.Auth;
-using System;
 using System.Collections.Generic;
 using System.Runtime.CompilerServices;
 using System.Text.Json;
 using System.Threading;
-using System.Threading.Tasks;
 
 namespace SlimAtp
 {
     partial class SlimAtpClientImpl
     {
-        async Task<JsonElement> ISlimAtpUserClient.GetUserAsync(IAzureTenant tenant, Guid id, ScalarRequestOptions? options, CancellationToken cancellationToken)
+        async IAsyncEnumerable<JsonElement> ISlimAtpUserClient.ListUserMachinesAsync(IAzureTenant tenant, string id, ListRequestOptions? options, [EnumeratorCancellation] CancellationToken cancellationToken)
         {
-            var link = BuildLink(options, $"users/{id}");
-
-            return await GetAsync(tenant, link, new RequestHeaderOptions(), cancellationToken).ConfigureAwait(false);
-        }
-
-        async IAsyncEnumerable<JsonElement> ISlimAtpUserClient.ListUsersAsync(IAzureTenant tenant, ListRequestOptions? options, [EnumeratorCancellation] CancellationToken cancellationToken)
-        {
-            var nextLink = BuildLink(options, "users");
+            var nextLink = BuildLink(options, $"users/{id}/machines");
 
             await foreach (var item in GetArrayAsync(tenant, nextLink, options, cancellationToken))
             {
