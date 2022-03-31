949643eefe9450f72cfd155fcfd3293fa72d7676
Jan Berdel <jan.berdel@glueckkanja-gab.com>
2022-03-31 10:44:45 +0200

    Formatting

--- SlimLib.Microsoft.Defender.AdvancedThreatProtection/ClientInterfaces/ISlimAtpMachineClient.cs
+++ SlimLib.Microsoft.Defender.AdvancedThreatProtection/ClientInterfaces/ISlimAtpMachineClient.cs
@@ -11,6 +11,7 @@ namespace SlimLib.Microsoft.Defender.AdvancedThreatProtection
         Task<JsonElement> GetMachineAsync(IAzureTenant tenant, string id, ScalarRequestOptions? options = default, CancellationToken cancellationToken = default);
 
         IAsyncEnumerable<JsonElement> ListMachinesAsync(IAzureTenant tenant, ListRequestOptions? options = default, CancellationToken cancellationToken = default);
+
         IAsyncEnumerable<JsonElement> ListMachineLogOnUsersAsync(IAzureTenant tenant, string id, ListRequestOptions? options = default, CancellationToken cancellationToken = default);
         IAsyncEnumerable<JsonElement> ListMachineSoftwareAsync(IAzureTenant tenant, string id, ListRequestOptions? options = default, CancellationToken cancellationToken = default);
         IAsyncEnumerable<JsonElement> ListMachineRecommendationsAsync(IAzureTenant tenant, string id, ListRequestOptions? options = default, CancellationToken cancellationToken = default);
